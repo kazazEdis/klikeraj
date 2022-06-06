a71f3100d0d43ab55ca06fe1dfbea6a019adc57f
edis <kazaz.edis@gmail.com>
2022-06-06 15:38:23 +0200

    generate initialCounters by screen width

--- src/components/CounterContainer.js
+++ src/components/CounterContainer.js
@@ -6,11 +6,25 @@ import Context from './Context';
 
 function CounterContainer(){
   let [id,setId] = useState(0);
- 
-  const [state, setState] = useState({counters: [<Counter id={id} key={nanoid()}/>]});
+  
+  
+  const bodyWidth = document.querySelector("body").clientWidth;
+
+  const initialCounters = [];
+  const counterWidth = 330;
+  const initialCounterNo = parseInt(bodyWidth / counterWidth) - 1;
+  for (let index = 0; index < initialCounterNo; index++) {
+    initialCounters.push(<Counter id={id} key={nanoid()}/>)
+  }
+
+  const styles = {
+    marginLeft: (bodyWidth % counterWidth) / 2,
+  }
+
+  const [state, setState] = useState({counters: initialCounters });
   return(
     <Context.Provider value={[state, setState]}>
-    <div id="CounterContainer" className="CounterContainer">
+    <div id="CounterContainer" className="CounterContainer" style={styles}>
       {state.counters}
       <div id="DummyBox" className="CounterBox count-btn" onClick={()=>{setId(id+1);setState({counters: [...state.counters,<Counter id={id} key={nanoid()}/>]})}}>+</div>
     </div>
